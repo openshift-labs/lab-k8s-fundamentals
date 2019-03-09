a8006d09383551e729709e5cf19b66104652b3b7
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-03-09 21:18:04 +1100

    Add Google analytics tracking code.

--- workshop/config.js
+++ workshop/config.js
@@ -1,5 +1,19 @@
+var google_analytics = `
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135921114-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-135921114-1');
+</script>
+`;
+
 var config = {
     site_title: 'Kubernetes Fundamentals',
+
+    analytics: google_analytics,
 
     variables: [
         {
