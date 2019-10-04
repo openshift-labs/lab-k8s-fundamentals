88a998d13d73755bb561680ee3f68add0839185c
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-10-04 15:22:09 +1000

    Update to latest workshop scripts etc.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,8 @@
 WORKSHOP_NAME=lab-k8s-fundamentals
 WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-k8s-fundamentals:master
+WORKSHOP_TITLE="Kubernetes Fundamentals"
+WORKSHOP_DESCRIPTION="A workshop introducing developers to Kubernetes"
+SPAWNER_PASSWORD=homeroom
 RESOURCE_BUDGET=medium
 MAX_SESSION_AGE=5400
 IDLE_TIMEOUT=300
